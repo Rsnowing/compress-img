7456e94d74482560391ae22c02b2c5d68954de83
llhe <helinglin@julanling.com>
2020-09-24 14:40:27 +0800

    成功啦

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
 ## 介绍
 ## TODO & DONE
-1. 压缩
-2. 非图片校验
-3. 返回file,base64, 文件名
-4. 错误处理
-5. ios exif图片翻转问题解决
-6. demo
+1. 压缩 ✅
+2. 非图片校验✅
+3. 返回file, base64, Blob, bloburl, 原图大小， 压缩后大小✅
+4. 错误处理✅
+5. ios exif图片翻转问题解决✅
+6. demo✅
+7. 成功回调，失败回调 ✅
 
+
+## 使用
+### 安装
+```js
+npm i for-compress 
+```
+### 页面使用
+```
+ <input type="file" placeholder="图片上传" id="upload" accept="image/*" />
+
+  import Compress from 'for-compress
+  document.getElementById('upload').addEventListener('change', event => {
+    new Compress(event.target.files[0], {
+      success: res => {
+          console.log(res);
+        },
+        fail: err => {
+          console.log(err);
+        }
+    })
+  })
+```
--- src/compress.js
+++ src/compress.js
@@ -1,18 +1,41 @@
-/* eslint-disable indent */
 /**
- * 压缩图片
+ * 说明
+ * 这是一个压缩图片的工具库
+ * 使用场景: 在浏览器上传图片前对图片压缩处理
+ * 解决了什么问题:
+ * 1. 通过canvas压缩
+ * 2. IOS13.4之前 h5手机拍照后的图片自动旋转，IOS13.4之后这个问题又被修复的兼容
+ * 3. 压缩后图可能会比原图大的问题
+ * 4. png图片转jpeg时，透明区域被填充成黑色 https://evestorm.github.io/posts/46911/
  */
-import EXIF from 'exif-js'
-// const EXIF = require('exif-js')
 
-function Compress() {}
+import EXIF from 'exif-js'
+
+const Compress = function (file, config) {
+  this.defaultConfig = {
+    quality: 0.8, // 图片质量 0 - 1
+    useOriginal: false, // 是否采用原图宽度高度压缩 默认false 暂不支持自定义
+    width: 800, // 压缩后图片最大宽度 不支持自定义
+    height: 1200, // 压缩后图片最大高度 不支持自定义
+    success: noop, // 成功回调
+    fail: noop // 失败回调
+  }
+  this.config = { ...this.defaultConfig, ...config }
+  this.file = file
+  this.ctx = null
+  this.resize = null
+  this.core(file)
+}
 
 /**
  * 校验图片
- * @param { 图片文件 } file
+ * @param { File } file
  */
 Compress.prototype.validate = function (file) {
   const imgReg = /image\/jpeg|image\/jpg|image\/png/
+  if (!document.createElement('canvas').getContext) {
+    throw new Error('浏览器不支持canvas')
+  }
   if (!file) {
     throw new Error('请传入图片!')
   }
@@ -23,55 +46,61 @@ Compress.prototype.validate = function (file) {
 
 /**
  * 图片压缩
- * @param { 图片文件 } file
+ * @param { File } file 图片文件
  */
 Compress.prototype.core = function (file) {
-  const _this = this
-  return new Promise(function (resolve, reject) {
-    try {
-      _this.validate(file)
-      let result = {}
-      result.originalSize = getfilesize(file.size) // 原图大小
-      let orientation = ''
-      EXIF.getData(file, function () {
-        EXIF.getAllTags(this)
-        orientation = EXIF.getTag(this, 'Orientation')
-      })
-      let fileReader = new FileReader()
-      fileReader.onload = function (e) {
-        let image = new Image()
-        image.src = e.target.result
-        image.onload = function () {
-          let expectWidth = this.naturalWidth
-          let expectHeight = this.naturalHeight
-          result.originalGeometry = { width: expectWidth, height: expectHeight } // 原图几何大小
-          if (this.naturalWidth > this.naturalHeight && this.naturalWidth > 800) {
-            expectWidth = 800
-            expectHeight = (expectWidth * this.naturalHeight) / this.naturalWidth
-          } else if (this.naturalHeight > this.naturalWidth && this.naturalHeight > 1200) {
-            expectHeight = 1200
-            expectWidth = (expectHeight * this.naturalWidth) / this.naturalHeight
-          }
-          result.compressedGeometry = { width: expectWidth, height: expectHeight } // 压缩图几何大小
-          let canvas = document.createElement('canvas')
-          let ctx = canvas.getContext('2d')
-          canvas.width = expectWidth
-          canvas.height = expectHeight
-          ctx.drawImage(this, 0, 0, expectWidth, expectHeight)
-          _this.getBase64(orientation, canvas).then(base64 => {
-            result.compressedSize = getfilesize(base64.length) // 压缩后大小
-            result.base64 = base64
-            result.file = base64ConverFile(base64, file.name)
-            result.originFile = file
-            resolve(result)
-          })
-        }
+  try {
+    this.validate(file)
+    let result = {} // 返回结果对象
+    result.originalSize = getfilesize(file.size) // 原图大小
+    let orientation = ''
+    EXIF.getData(file, function () {
+      EXIF.getAllTags(this)
+      orientation = EXIF.getTag(this, 'Orientation')
+    })
+    const _this = this
+    let fileReader = new FileReader()
+    fileReader.onload = function (e) {
+      let image = new Image()
+      image.src = e.target.result
+
+      image.onerror = function () {
+        _this.config.fail('加载图片文件失败')
       }
-      fileReader.readAsDataURL(file)
-    } catch (error) {
-      reject(error)
+
+      image.onload = function () {
+        const originalWidth = this.naturalWidth
+        const originalHeight = this.naturalHeight
+        result.originalReSize = { width: originalWidth, height: originalHeight } // 原图几何大小
+        _this.getResize(originalWidth, originalHeight) // 获取压缩后的图片几何大小
+        let { compressedWidth, compressedHeight } = _this.resize
+        result.resize = { width: compressedWidth, height: compressedHeight } // 压缩图几何大小
+        let canvas = document.createElement('canvas')
+        let ctx = canvas.getContext('2d')
+        this.ctx = ctx
+        canvas.width = compressedWidth
+        canvas.height = compressedHeight
+
+        // 在canvas绘制前填充白色背景
+        ctx.fillStyle = '#fff'
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+        ctx.drawImage(this, 0, 0, compressedWidth, compressedHeight)
+        _this.getBase64(orientation, canvas).then(base64 => {
+          result.size = getfilesize(base64.length) // 压缩后大小
+          result.base64 = base64
+          result.file = base64ConverFile(base64, file.name)
+          result.originFile = file
+          result.blob = dataURLtoBlob(base64)
+          result.blobUrl = URL.createObjectURL(result.blob)
+          _this.config.success(result)
+        })
+      }
     }
-  })
+    fileReader.readAsDataURL(file)
+  } catch (error) {
+    this.config.fail(error)
+  }
 }
 
 /**
@@ -79,30 +108,17 @@ Compress.prototype.core = function (file) {
  */
 Compress.prototype.getBase64 = function (orientation, canvas) {
   let base64 = null
+  const _this = this
   return new Promise((resolve, reject) => {
     try {
       detectImageAutomaticRotation().then(res => {
         const isAutomaticRotation = res
         // 如果是浏览器不会自动回正图片方向的iphone 则旋转方向
         if (navigator.userAgent.match(/iphone/i && !isAutomaticRotation)) {
-          //如果方向角不为1，都需要进行旋转
-          if (orientation !== '' && orientation !== 1) {
-            switch (orientation) {
-              case 6: // 需要顺时针（向左）90度旋转
-                this.rotateImg(this, 'left', canvas)
-                break
-              case 8: // 需要逆时针（向右）90度旋转
-                this.rotateImg(this, 'right', canvas)
-                break
-              case 3: // 需要180度旋转
-                this.rotateImg(this, 'right', canvas) //转两次
-                this.rotateImg(this, 'right', canvas)
-                break
-            }
-          }
-          base64 = canvas.toDataURL('image/jpeg', 0.8)
+          this.rotate(this, orientation)
+          base64 = canvas.toDataURL('image/jpeg', _this.config.quality)
         } else {
-          base64 = canvas.toDataURL('image/jpeg', 0.8)
+          base64 = canvas.toDataURL('image/jpeg', _this.config.quality)
         }
         resolve(base64)
       })
@@ -112,57 +128,74 @@ Compress.prototype.getBase64 = function (orientation, canvas) {
   })
 }
 
-/**
- * 对图片旋转处理
- */
-Compress.prototype.rotateImg = function (img, direction, canvas) {
-  // 最小与最大旋转方向，图片旋转4次后回到原方向
-  let min_step = 0
-  let max_step = 3
-  if (img == null) return
-  // img的高度和宽度不能在img元素隐藏后获取，否则会出错
-  let height = img.height
-  let width = img.width
-  let step = 2
-  if (step == null) {
-    step = min_step
-  }
-  if (direction == 'right') {
-    step++
-    // 旋转到原位置，即超过最大值
-    step > max_step && (step = min_step)
-  } else {
-    step--
-    step < min_step && (step = max_step)
-  }
-  // 旋转角度以弧度值为参数
-  let degree = (step * 90 * Math.PI) / 180
-  let ctx = canvas.getContext('2d')
-  canvas.width = width
-  canvas.height = height
-  switch (step) {
-    case 0:
-      ctx.drawImage(img, 0, 0)
-      break
-    case 1:
-      ctx.rotate(degree)
-      ctx.drawImage(img, 0, -height)
-      break
-    case 2:
-      ctx.rotate(degree)
-      ctx.drawImage(img, -width, -height)
-      break
+Compress.prototype.rotate = function (img, orientation) {
+  // 调整为正确方向
+  const ctx = this.ctx
+  const resize = this.resize
+  /* eslint-disable indent */
+  switch (orientation) {
     case 3:
-      ctx.rotate(degree)
-      ctx.drawImage(img, -width, 0)
+      ctx.rotate((180 * Math.PI) / 180)
+      ctx.drawImage(img, -resize.width, -resize.height, resize.width, resize.height)
       break
+    case 6:
+      ctx.rotate((90 * Math.PI) / 180)
+      ctx.drawImage(img, 0, -resize.width, resize.height, resize.width)
+      break
+    case 8:
+      ctx.rotate((270 * Math.PI) / 180)
+      ctx.drawImage(img, -resize.height, 0, resize.height, resize.width)
+      break
+
+    case 2:
+      ctx.translate(resize.width, 0)
+      ctx.scale(-1, 1)
+      ctx.drawImage(img, 0, 0, resize.width, resize.height)
+      break
+    case 4:
+      ctx.translate(resize.width, 0)
+      ctx.scale(-1, 1)
+      ctx.rotate((180 * Math.PI) / 180)
+      ctx.drawImage(img, -resize.width, -resize.height, resize.width, resize.height)
+      break
+    case 5:
+      ctx.translate(resize.width, 0)
+      ctx.scale(-1, 1)
+      ctx.rotate((90 * Math.PI) / 180)
+      ctx.drawImage(img, 0, -resize.width, resize.height, resize.width)
+      break
+    case 7:
+      ctx.translate(resize.width, 0)
+      ctx.scale(-1, 1)
+      ctx.rotate((270 * Math.PI) / 180)
+      ctx.drawImage(img, -resize.height, 0, resize.height, resize.width)
+      break
+    default:
+      ctx.drawImage(img, 0, 0, resize.width, resize.height)
   }
+}
+
+/**
+ * 获取压缩后图片宽高
+ */
+Compress.prototype.getResize = function (originalWidth, originalHeight) {
+  const { width, height } = this.config
+  let compressedWidth = originalWidth,
+    compressedHeight = originalHeight
+  if (originalWidth >= originalHeight && this.originalWidth > width) {
+    compressedWidth = width
+    compressedHeight = (compressedWidth * originalHeight) / originalWidth
+  } else if (originalHeight >= originalWidth && originalHeight > height) {
+    compressedHeight = height
+    compressedWidth = (compressedHeight * originalWidth) / originalHeight
+  }
+  this.resize = { compressedWidth, compressedHeight }
 }
 
 /**
  * base64Url转file文件域
  * @param {String} base64Url 必须，图片base64字符串
- * @param {String} name 可选，图片名称，默认随机生成
+ * @param {String} filename
  */
 function base64ConverFile(base64Url, filename) {
   let arr = base64Url.split(','),
@@ -174,6 +207,19 @@ function base64ConverFile(base64Url, filename) {
     u8arr[n] = bstr.charCodeAt(n)
   }
   return new File([u8arr], filename, { type: mime })
+}
+
+/**
+ * base64 -> blob
+ * @param {String} base64Url
+ */
+function dataURLtoBlob(base64Url) {
+  let binary = atob(base64Url.split(',')[1])
+  let array = []
+  for (let i = 0; i < binary.length; i++) {
+    array.push(binary.charCodeAt(i))
+  }
+  return new Blob([new Uint8Array(array)], { type: 'image/jpeg' })
 }
 
 /**
@@ -205,7 +251,7 @@ function detectImageAutomaticRotation() {
 
 /**
  * 文件大小格式化
- * @param { 文件字节数 } size
+ * @param { Number } size 文件字节数
  */
 function getfilesize(size) {
   if (!size) return ''
@@ -215,6 +261,10 @@ function getfilesize(size) {
   if (size < Math.pow(num, 3)) return (size / Math.pow(num, 2)).toFixed(2) + 'M' // M
   if (size < Math.pow(num, 4)) return (size / Math.pow(num, 3)).toFixed(2) + 'G' // G
   return (size / Math.pow(num, 4)).toFixed(2) + 'T' // T
+}
+
+function noop() {
+  return () => {}
 }
 
 export default Compress
